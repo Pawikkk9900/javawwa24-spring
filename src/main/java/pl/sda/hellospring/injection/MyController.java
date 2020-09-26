e15fc12d1451146d32b9f0ed0f5ab7db5fa66388
OPPawel <oppawell@gmail.com>
2020-09-26 13:45:08 +0200

    [change] - playing with bean and logs

---
+++ src/main/java/pl/sda/hellospring/injection/MyController.java
@@ -0,0 +1,15 @@
+package pl.sda.hellospring.injection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+public class MyController {
+
+    public MyController() {
+      //  log.debug("pl.sda.hellospring.injection.MyController");
+        log.trace("pl.sda.hellospring.injection.MyController");
+        log.info("pl.sda.hellospring.injection.MyController");
+    }
+}
